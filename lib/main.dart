9bfc699d3d8bd26c85f820b2df71294017dcd8c9
juanpablo-devia <juan.devia01@uceva.edu.co>
2026-04-01 13:43:56 -0500

    Revert "Uu 13 gestion de perfil y rol"

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'theme/app_theme.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const UniConnectApp());
 }
 
