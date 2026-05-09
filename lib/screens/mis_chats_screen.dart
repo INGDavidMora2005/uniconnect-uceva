d7833f4308982d5a1a7f5b54b93e5eccdfebdb11
Juan Diego Rodriguez <juan.rodriguez16@uceva.edu.co>
2026-05-09 09:27:14 -0500

    feat(admin): add chats history tab to moderation panel [UU-67]

--- lib/screens/mis_chats_screen.dart
+++ lib/screens/mis_chats_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
